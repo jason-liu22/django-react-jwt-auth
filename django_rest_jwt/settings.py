2cf6d1d4051d1ae4e54e40655fd3780cd6141344
pydjangodev <pydjangodev@gmail.com>
2023-08-08 03:29:25 +0800

    add ci/cd workflow

--- django_rest_jwt/settings.py
+++ django_rest_jwt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(o+m&u9vzp2t!us)a+nsco8%-!6$)i%9+yk=e-3og+=h4o9^np"
+SECRET_KEY = os.environ.get("SECRET_KEY", "secret_key_required")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ENVIRONMENT = os.environ.get("ENVIRONMENT")
