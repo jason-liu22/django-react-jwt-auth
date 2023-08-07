45e9014881bed578e23651c26796f350bb798bed
pydjangodev <pydjangodev@gmail.com>
2023-08-08 03:45:14 +0800

    add nginx

--- django_rest_jwt/settings.py
+++ django_rest_jwt/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 
 MEDIA_URL = "/media/"
 STATIC_URL = "/static/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
